00c2ea044f1a92684e75be8e9be083b645d6e3ba
Filipe Costa <filipebnks@gmail.com>
2020-11-19 12:09:01 -0300

    Entendendo Objetos

---
+++ objects.js
@@ -0,0 +1,14 @@
+/*
+let nome = 'Filipe';
+let idade = 20;
+let estaAprovado = true;
+let sobrenome = undefined;
+*/
+
+// Definindo o Objeto
+let pessoa = {
+    nome:['Emily','Filipe'],
+    idade:[20, 21],
+    estaAprovado:[true, false],
+    sobrenome: undefined 
+};
